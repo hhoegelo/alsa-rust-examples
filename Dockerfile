d407641ad926434045538c7c32da364873bcd849
Henry Hoegelow <henry@hoegelow.com>
2022-12-20 10:48:22 +0100

    WIP

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM rustembedded/cross:armv7-unknown-linux-gnueabihf-0.2.1
 RUN apt-get update
 RUN dpkg --add-architecture armhf && \
     apt-get update && \
-    apt-get install --assume-yes libssl-dev:armhf libasound2-dev:armhf
+    apt-get install --assume-yes libssl-dev:armhf libasound2-dev:armhf pkg-config:armhf alsa:armhf
+RUN apt-get install --assume-yes libasound-dev:armhf
 
 ENV PKG_CONFIG_LIBDIR_armv7_unknown_linux_gnueabihf=/usr/lib/arm-linux-gnueabihf/pkgconfig
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use alsa::pcm::{Access, Format, Frames, HwParams, PCM};
 use alsa::{Direction, ValueOr};
 use clap::Parser;
 use std::sync::Arc;
-use std::time::{Duration, Instant};
-use std::{thread, time};
 
 #[derive(Parser, Debug)]
 struct CommandLineArgs {
@@ -11,13 +9,13 @@ struct CommandLineArgs {
     output: String,
 
     #[clap(short, long, default_value_t = 44100)]
-    rate: u32,
+    rate: usize,
 
     #[clap(short, long, default_value_t = 2)]
-    num_periods: u32,
+    num_periods: usize,
 
     #[clap(short, long, default_value_t = 384)]
-    period_size: i64,
+    period_size: usize,
 }
 
 fn main() {
@@ -27,56 +25,45 @@ fn main() {
     hwp.set_channels(2).unwrap();
     hwp.set_period_size(args.period_size as Frames, alsa::ValueOr::Nearest)
         .unwrap();
-    hwp.set_periods(args.num_periods, alsa::ValueOr::Nearest)
+    hwp.set_periods(args.num_periods as u32, alsa::ValueOr::Nearest)
         .unwrap();
-    hwp.set_rate(args.rate, ValueOr::Nearest).unwrap();
+    hwp.set_rate(args.rate as u32, ValueOr::Nearest).unwrap();
     hwp.set_format(Format::s32()).unwrap();
     hwp.set_access(Access::RWInterleaved).unwrap();
     pcm.hw_params(&hwp).unwrap();
 
     let swp = pcm.sw_params_current().unwrap();
-    swp.set_start_threshold((args.period_size * args.num_periods as i64) as Frames)
+    swp.set_start_threshold((args.period_size * args.num_periods) as Frames)
         .unwrap();
     pcm.sw_params(&swp).unwrap();
-    
-    let dev = pcm.io_i32();
-    let output = dev.unwrap();
-    let mut buf = vec![0i32; args.period_size as usize];
 
-    // play silence first
-    output.writei(&buf[..]).unwrap();
+    let output = pcm.io_i32().unwrap();
+    let buf = vec![0i32; args.period_size];
+
+    output.writei(&buf).unwrap();
     pcm.start().unwrap();
 
-    let mut start_time = Instant::now();
-    let mut earlier = Instant::now();
+    let mut last_delay = pcm.delay().unwrap_or(0);
 
     loop {
-        let later = Instant::now();
-        let duration = later.duration_since(earlier).as_millis();
+        let delay = pcm.delay().unwrap_or(0);
 
-        if duration > 1000 {
-            earlier = later;
-            buf[0] = i32::MAX;
-            buf[1] = -i32::MAX;
-            buf[2] = i32::MAX;
-        }
-        else {
-            buf[0] = 0;
-            buf[1] = 0;
-            buf[2] = 0;
+        if delay != last_delay {
+            println!("Delay changed: {}", delay);
+            last_delay = delay;
         }
 
-        if later.duration_since(start_time).as_secs() > 5 {
-           // thread::sleep(time::Duration::from_millis(50));
-            start_time = later;
-        }
-
-        match output.writei(&buf[..]) {
+        match output.writei(&buf) {
             Err(e) => {
                 println!("recover from buffer underrun");
                 pcm.try_recover(e, true).unwrap();
-            },
-            Ok(_num_frames) => {}
+            }
+            Ok(num_frames) => {
+                if num_frames == 0 {
+                    println!("have to re-start");
+                    pcm.start().unwrap();
+                }
+            }
         }
     }
 }
